122e262ef51d5b536b55b637a79bd2b27922bbce
trilokshankar <trilokshankar4@gmail.com>
2024-07-26 03:20:48 +0530

    commit

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname) });
 });
 
+app.get('/api',async(req,res)=>{
+     console.log(req._parsedUrl.query)
+   let url="https://newsapi.org/v2/everything?"+req._parsedUrl.query
+   let r=await axios(url)
+   let a=r.data
+   res.json(a)
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
